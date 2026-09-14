8a383960aea02762bdd9061d8719689705cacc14
Diegolox <diegolopezesteban2k@gmail.com>
2026-09-14 13:45:20 +0200

    Separación de lectura y escritura del ADC
    Se ha movido el ADC a una librería privada nueva

--- P1_3_INTERRUPCION_Y_ADC/src/main.cpp
+++ P1_3_INTERRUPCION_Y_ADC/src/main.cpp
@@ -1,35 +1,24 @@
 #include <Arduino.h>
 #include "timer.h"
+#include "adc.h"
 
-#define ADC_PIN A0
+
 #define PERIODO_TIMER_HW 1000000 // f reloj 1 MHz 
-#define PWM_max 200
 
-volatile int dutty = 0; //[0-255]
-
-int leerADC(int pin);
 
 void setup()
 {
   Serial.begin(115200);
   iniciarTimer(PERIODO_TIMER_HW);
-  analogReadResolution(12); // Resolución ADC n bits, en este caso 4095
+  inicializarADC();
+  inicializarPWM();
 }
 
 void loop()
 {
   if (hayInterrupcionTimer())
   {
-    dutty = leerADC(ADC_PIN);
-
-    char buffer[40];
-
-    snprintf(buffer, sizeof(buffer), "El dutty de la PWM es: %d", dutty);
-
-    Serial.println(buffer);
+    ADC_to_PWM();
   }
 }
 
-int leerADC(int pin) {
-  return (analogRead(pin) * 255L) / 4095;
-}
