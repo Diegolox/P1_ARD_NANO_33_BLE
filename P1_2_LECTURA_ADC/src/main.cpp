551b1c94ef480e3a7ad43b5e08b260b2eacdd81d
Diegolox <diegolopezesteban2k@gmail.com>
2026-09-14 09:51:21 +0200

    lee tensión
    Se actualiza el código para que lea la tensión del pin en vez de solo el valor del ADC

--- P1_2_LECTURA_ADC/src/main.cpp
+++ P1_2_LECTURA_ADC/src/main.cpp
@@ -3,18 +3,18 @@
 #define ADC_PIN A0
 #define T 1000
 
-int lecturaADC(int pin);
+float leerTension(int pin);
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  Serial.print("Valor ADC: ");
-  Serial.println(lecturaADC(ADC_PIN));
+  Serial.print("Tensión: ");
+  Serial.println(leerTension(ADC_PIN));
   delay(T);
 }
 
-int lecturaADC(int pin) {
-  return analogRead(pin);
+float leerTension(int pin) {
+  return (analogRead(pin) * 3.3) / 1023;
 }
