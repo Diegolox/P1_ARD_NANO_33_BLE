9b6c48a2dd60804eb2e11ec3278953215e25ae1d
Diegolox <diegolopezesteban2k@gmail.com>
2026-09-14 12:56:43 +0200

    añadido del cálculo del dutty
    Ahora con el reloj se lee el ADC de forma periódica y se calcula el dutty

--- P1_3_INTERRUPCION_Y_ADC/src/main.cpp
+++ P1_3_INTERRUPCION_Y_ADC/src/main.cpp
@@ -3,20 +3,33 @@
 
 #define ADC_PIN A0
 #define PERIODO_TIMER_HW 1000000 // f reloj 1 MHz 
+#define PWM_max 200
+
+volatile int dutty = 0; //[0-255]
+
+int leerADC(int pin);
 
 void setup()
 {
   Serial.begin(115200);
   iniciarTimer(PERIODO_TIMER_HW);
+  analogReadResolution(12); // Resolución ADC n bits, en este caso 4095
 }
 
 void loop()
 {
   if (hayInterrupcionTimer())
   {
-      int lectura = analogRead(ADC_PIN);
+    dutty = leerADC(ADC_PIN);
 
-      Serial.print("Lectura ADC: ");
-      Serial.println(lectura);
+    char buffer[40];
+
+    snprintf(buffer, sizeof(buffer), "El dutty de la PWM es: %d", dutty);
+
+    Serial.println(buffer);
   }
+}
+
+int leerADC(int pin) {
+  return (analogRead(pin) * 255L) / 4095;
 }
