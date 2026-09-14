4502d47718d0b9fe6e77bcbdf8b2bf76c1fb78e2
Diegolox <diegolopezesteban2k@gmail.com>
2026-09-14 10:52:54 +0200

    Actualización ADC
    Se ha actualizado para añadir el sprintf y se ha cambiado de float a int para que sea más eficiente.

--- P1_2_LECTURA_ADC/src/main.cpp
+++ P1_2_LECTURA_ADC/src/main.cpp
@@ -3,18 +3,26 @@
 #define ADC_PIN A0
 #define T 1000
 
-float leerTension(int pin);
+int tension = 0;
+
+int leerTension(int pin);
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  Serial.print("Tensión: ");
-  Serial.println(leerTension(ADC_PIN));
+  tension = leerTension(ADC_PIN);
+
+  char buffer[40];
+
+  snprintf(buffer, sizeof(buffer), "La tension del ADC es: %d mV", tension);
+
+  Serial.println(buffer);
+
   delay(T);
 }
 
-float leerTension(int pin) {
-  return (analogRead(pin) * 3.3) / 1023;
+int leerTension(int pin) {
+  return (analogRead(pin) * 3300L) / 1023;
 }
--- P1_3_INTERRUPCION_Y_ADC/src/main.cpp
+++ P1_3_INTERRUPCION_Y_ADC/src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 
+//Constantes globales
 #define ADC_PIN A0
 #define T 1000
 
+// Variables globales
+float tension = 0.0;
+
+// Prototipos de funciones
 float leerTension(int pin);
 
 void setup() {
@@ -10,8 +15,10 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Tensión: ");
-  Serial.println(leerTension(ADC_PIN));
+  tension = leerTension(ADC_PIN);
+  char buffer[40];
+  sprintf(buffer, "La tensión del adc es: %s V", tension);
+  Serial.println(buffer);
   delay(T);
 }
 
