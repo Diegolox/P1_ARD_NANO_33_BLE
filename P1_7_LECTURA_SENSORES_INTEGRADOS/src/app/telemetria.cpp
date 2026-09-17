2a53e559550886d966251b59479c255e8da6f9af
Diegolox <diegolopezesteban2k@gmail.com>
2026-09-17 22:39:52 +0200

    preparación de envio continuo de datos del IMU

--- P1_7_LECTURA_SENSORES_INTEGRADOS/src/app/telemetria.cpp
+++ P1_7_LECTURA_SENSORES_INTEGRADOS/src/app/telemetria.cpp
@@ -7,6 +7,8 @@ Vector3D aceleracion;
 Vector3D giroscopo;
 Vector3D magnetometro;
 
+float lecturasIMU[5][9];
+
 void printIMU(){
   char buffer[120];
   leerMagnetometro(magnetometro);
@@ -47,4 +49,43 @@ void sendAce_I2C(uint8_t direccion){
   leerAcelerometro(aceleracion);
   snprintf(buffer, sizeof(buffer), "Ace X: %.4f, Y: %.4f, Z: %.4f", aceleracion.x, aceleracion.y, aceleracion.z);
   escribirI2C(direccion, buffer);
+}
+
+void guardarPaqueteIMU(){
+  for(int i = 0; i <5; i++){
+  leerAcelerometro(aceleracion);
+  leerGiroscopo(giroscopo);
+  leerMagnetometro(magnetometro);
+
+  lecturasIMU[i][0] = aceleracion.x;
+  lecturasIMU[i][1] = aceleracion.y;
+  lecturasIMU[i][2] = aceleracion.z;
+
+  lecturasIMU[i][3] = giroscopo.x;
+  lecturasIMU[i][4] = giroscopo.y;
+  lecturasIMU[i][5] = giroscopo.z;
+
+  lecturasIMU[i][6] = magnetometro.x;
+  lecturasIMU[i][7] = magnetometro.y;
+  lecturasIMU[i][8] = magnetometro.z;
+  delay(200); // Se podría hacer de otra forma pero esto es simple y cumple el propósito
+  }
+}
+
+void enviarPaqueteIMU(uint8_t direccion){
+  char buffer[100];
+  for(int i = 0; i <5; i++){
+
+    // envia aceleracion i
+    snprintf(buffer, sizeof(buffer), "A%d:%2.f;%2.f;%2.f;", i, lecturasIMU[i][0], lecturasIMU[i][1], lecturasIMU[i][2]);
+    escribirI2C(direccion, buffer);
+
+    // envia giroscopo i
+    snprintf(buffer, sizeof(buffer), "G%d:%2.f;%2.f;%2.f;", i, lecturasIMU[i][3], lecturasIMU[i][4], lecturasIMU[i][5]);
+    escribirI2C(direccion, buffer);
+
+    // envia magnetometro i
+    snprintf(buffer, sizeof(buffer), "M%d:%2.f;%2.f;%2.f;", i, lecturasIMU[i][6], lecturasIMU[i][7], lecturasIMU[i][8]);
+    escribirI2C(direccion, buffer);
+  }
 }
